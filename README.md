aac9e3b168c9fc844c07d7683530a4921acafc28
Della Silvira <dellasilvira24@gmail.com>
2025-06-12 17:26:40 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+nama : Della silvira
+
+nim: 607062300095
---
+++ app/src/main/java/com/dellas0095/assesmobpro3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.dellas0095.assesmobpro3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dellas0095.assesmobpro3.ui.theme.Assesmobpro3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Assesmobpro3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Assesmobpro3Theme {
+        Greeting("Android")
+    }
+}
